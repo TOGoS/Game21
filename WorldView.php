929c14a64493de3b1094ce9f99828583443597e7
TOGoS <togos00@gmail.com>
2016-03-11 19:17:28 -0600

    Add WorldView.php, remove old table-based TileView crap.

---
+++ WorldView.php
@@ -0,0 +1,53 @@
+<?php
+	require_once 'lib.php';
+	if( !isset($mode ) ) $mode = 'normal';
+	if( !isset($title) ) $title = ($mode === 'demo' ? 'Shapes!' : 'Shape Editor');
+	if( !isset($inlineResources) ) $inlineResources = ($mode === 'demo');
+?>
+<html>
+<head>
+<meta charset="utf-8"/>
+<style>
+.world-view-region {
+	width: 100%;
+	height: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+.world-view-canvas {
+	background: black;
+	width: 640px;
+	height: 480px;
+	image-rendering: pixelated;
+}
+</style>
+</head>
+<body style="background: rgb(32,0,0)">
+
+<div class="world-view-region">
+<canvas id="world-view-canvas" class="world-view-canvas" width="320" height="240">
+</canvas>
+</div>
+
+<?php
+	$requireJsFiles = [
+		'ShapeSheet.js',
+		'ShapeSheetTransforms.js',
+		'ShapeSheetRenderer.js',
+		'ShapeSheetUtil.js',
+		'CanvasWorldView.js'
+	];
+	
+	require_js($requireJsFiles, $inlineResources);
+?>
+<script>//<![CDATA[
+(function() {
+	var wv = new CanvasWorldView();
+	wv.initUi(document.getElementById('world-view-canvas'));
+	wv.runDemo();
+})();
+//]]></script>
+
+</body>
+</html>
