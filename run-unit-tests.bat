2f3c35e120de7ade26436e913c16461fd8c9f085
TOGoS <togos00@gmail.com>
2016-04-23 03:54:25 -0500

    Fix run-unit-tests.bat to reference new location of tsconfig.

--- run-unit-tests.bat
+++ run-unit-tests.bat
@@ -1,1 +1,1 @@
-tsc -p game21libs.cjs.es5.tsconfig.json && find target/cjs -name "*Test.js" | xargs node
+tsc -p src/main/ts/game21libs.cjs.es5.tsconfig.json && find target/cjs -name "*Test.js" | xargs node
