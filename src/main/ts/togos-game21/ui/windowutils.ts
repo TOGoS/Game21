961eea423fe961433f4c3c571650ccd574765bc3
TOGoS <togos00@gmail.com>
2016-07-11 11:59:43 -0500

    Add missing windowutils.ts.

---
+++ src/main/ts/togos-game21/ui/windowutils.ts
@@ -0,0 +1,17 @@
+import KeyedList from '../KeyedList';
+
+export function getQueryStringValues(qs:string = window.location.search.substr(1)):KeyedList<string> {
+	const qsParts = qs.split('&');
+	const vals:KeyedList<string> = {};
+	for( let p in qsParts ) {
+		const q = qsParts[p];
+		if( q == '' ) continue;
+		const kv = q.split('=',2);
+		if( kv.length == 1 ) {
+			vals[kv[0]] = kv[0];
+		} else {
+			vals[kv[1]] = kv[1];
+		}
+	}
+	return vals;
+}
