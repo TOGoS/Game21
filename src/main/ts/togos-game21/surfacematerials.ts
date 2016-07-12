9d86c66b131ca55863ae17e30ea7ffd31fd92f87
TOGoS <togos00@gmail.com>
2016-07-12 02:12:13 -0500

    Define some foliage and bark materials.

--- src/main/ts/togos-game21/surfacematerials.ts
+++ src/main/ts/togos-game21/surfacematerials.ts
@@ -1,7 +1,7 @@
 import KeyedList from './KeyedList';
 import { deepFreeze } from './DeepFreezer';
 import SurfaceColor from './SurfaceColor';
-import SurfaceMaterial from './SurfaceMaterial';
+import SurfaceMaterial, { SurfaceMaterialLayer } from './SurfaceMaterial';
 
 export const MATERIAL_MAP_SIZE = 256;
 
@@ -67,17 +67,43 @@ const ps1Ref    = 'urn:uuid:b29bc460-4c9d-43e3-a3d2-f9a4114ac9f9';
 const ps2Ref    = 'urn:uuid:1ba047d0-6fdf-4bd1-bf94-db57c77cfd74';
 const ps3Ref    = 'urn:uuid:624f532a-b37b-43be-aedf-d1d3a361191b';
 const folg0Ref  = 'urn:uuid:c7a19722-d114-4b76-8c6a-51686ccf56aa';
+const folg1Ref  = 'urn:uuid:80d1a0c5-d621-40b4-94fd-21453c1d70cd';
+const folg2Ref  = 'urn:uuid:f70c6929-68ba-4c4e-9ae9-254b9dcb609e';
+const folg3Ref  = 'urn:uuid:d0c90575-fba9-41f1-888b-0339f0f4a498';
+const wood0Ref  = 'urn:uuid:4b73a4f5-d717-4907-9435-3004e0a5087d';  
+const wood1Ref  = 'urn:uuid:de11e708-17d9-46ec-a58c-58cb897b8b47';
+const wood2Ref  = 'urn:uuid:ea0f7659-4e74-4b29-9324-9d40f27ce897';
+const wood3Ref  = 'urn:uuid:5e820a3a-6457-4550-8909-2cff51b8b1a6';
+const bark0Ref  = 'urn:uuid:ea6ae5c8-5c01-49b9-9d59-4d74a7a15079';
+const bark1Ref  = 'urn:uuid:9e2aea33-9210-49ad-b6b1-85ea8587a922';
+const bark2Ref  = 'urn:uuid:30a6bfe8-7afa-47da-a651-3e7fa7884ab0';
+const bark3Ref  = 'urn:uuid:603f6da3-f6ce-4bd8-87c2-01556cea502a';
+const bark4Ref  = 'urn:uuid:15ac9b6e-6242-4e97-bf91-02d881d93c05';
 /*
 CHECK OUT THSI AWSUM WEB SIGHT https://www.uuidgenerator.net/;
-'urn:uuid:80d1a0c5-d621-40b4-94fd-21453c1d70cd';
-'urn:uuid:f70c6929-68ba-4c4e-9ae9-254b9dcb609e';
-'urn:uuid:d0c90575-fba9-41f1-888b-0339f0f4a498';
-'urn:uuid:4b73a4f5-d717-4907-9435-3004e0a5087d';
-'urn:uuid:de11e708-17d9-46ec-a58c-58cb897b8b47';
-'urn:uuid:ea0f7659-4e74-4b29-9324-9d40f27ce897';
-'urn:uuid:5e820a3a-6457-4550-8909-2cff51b8b1a6';
-'urn:uuid:ea6ae5c8-5c01-49b9-9d59-4d74a7a15079';
+
+
+;
+;
+'urn:uuid:3cf9be1b-f6c0-483a-b1e0-1af390f35974';
+'urn:uuid:5b8d89b1-0f26-4987-8c56-218c1435e639';
+'urn:uuid:f9595a26-ae24-4948-84c7-6928d60ff52b';
+'urn:uuid:3184d81d-843f-4a24-b7f8-6dd0c4c014b0';
+'urn:uuid:36f70539-70b0-479b-8739-eb528f1da3f6';
+'urn:uuid:bc1e5d12-6b48-45f5-879c-ccae533639f7';
 */
+
+function rgbsc(r:number,g:number,b:number,a:number=255):SurfaceColor {
+	return new SurfaceColor(r/255,g/255,b/255,a/255);
+}
+
+function sml(ruff:number,r:number,g:number,b:number,a:number=255):SurfaceMaterialLayer {
+	return { ruffness:ruff, diffuse:rgbsc(r,g,b,a) }
+}
+
+function smat(title:string, ...layers:SurfaceMaterialLayer[] ) : SurfaceMaterial {
+	return { title: title, layers: layers };
+}
 
 export const DEFAULT_MATERIALS:KeyedList<SurfaceMaterial> = {
 	[noneRef]: NONE,
@@ -167,18 +193,54 @@ export const DEFAULT_MATERIALS:KeyedList<SurfaceMaterial> = {
 		} ],
 	},
 	[folg0Ref]: {
-		title: "foliage 0",
+		title: "light green foliage",
 		layers: [
 			{
 				ruffness: 1.5,
-				diffuse: new SurfaceColor(0.0,0.40,0.00),
+				diffuse: rgbsc(104,111,73),
 			},
 			{
 				ruffness: 0.5,
-				diffuse: new SurfaceColor(0.55,1.00,0.25,0.25)
+				diffuse: rgbsc(153,229,130,64),
 			},
 		],
 	},
+	[folg1Ref]: {
+		title: "medium green foliage",
+		layers: [
+			{
+				ruffness: 1.5,
+				diffuse: rgbsc(52,133,28),
+			},
+			{
+				ruffness: 0.5,
+				diffuse: rgbsc(61,157,3,64),
+			},
+		],
+	},
+	[folg2Ref]: {
+		title: "vibrant yellow-green foliage",
+		layers: [
+			{
+				ruffness: 1.2,
+				diffuse: rgbsc(124,195,23),
+			}
+		],
+	},
+	[folg3Ref]: {
+		title: "dark blue-green foliage",
+		layers: [
+			{
+				ruffness: 1.2,
+				diffuse: rgbsc(25,109,62),
+			}
+		],
+	},
+	[bark0Ref]: smat("light gray bark", sml(1.1, 173,146,125)),
+	[bark1Ref]: smat("medium gray bark", sml(1.1, 124,111,102)),
+	[bark2Ref]: smat("dark gray bark", sml(1.1, 75,66,59)),
+	[bark3Ref]: smat("medium brown bark", sml(1.1, 155,90,61)),
+	[bark4Ref]: smat("medium brown bark", sml(1.0, 66,28,12)),
 };
 deepFreeze(DEFAULT_MATERIALS, true);
 
@@ -197,7 +259,20 @@ export const DEFAULT_MATERIAL_PALETTE:Array<string> = deepFreeze([
 	ps1Ref,
 	ps2Ref,
 	ps3Ref,
+	// 12-15: foliage
 	folg0Ref,
+	folg1Ref,
+	folg2Ref,
+	folg3Ref,
+	// 16-23: bark
+	bark0Ref,
+	bark1Ref,
+	bark2Ref,
+	bark3Ref,
+	bark4Ref,
+	null,
+	null,
+	null
 ]);
 
 export const DEFAULT_MATERIAL_MAP:MaterialMap = deepFreeze(paletteToMap(DEFAULT_MATERIAL_PALETTE, DEFAULT_MATERIALS));
