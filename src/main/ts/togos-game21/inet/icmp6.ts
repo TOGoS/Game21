4490f81d2c0bf8865c69f12a459892244498aea7
TOGoS <togos00@gmail.com>
2016-08-15 19:31:23 -0500

    [untested] router advertisement packet assembly.

--- src/main/ts/togos-game21/inet/icmp6.ts
+++ src/main/ts/togos-game21/inet/icmp6.ts
@@ -1,9 +1,15 @@
 import IP6Address from './IP6Address';
+import PacketDecodeError from './PacketDecodeError';
 import checksumming, { tempDv, tempU8a, uint32ToU8a, initIp6PseudoHeaderChecksumming } from './checksumming';
 
 export const ICMP_PROTOCOL_NUMBER = 58;
 export const ICMP_TYPE_PING = 128;
 export const ICMP_TYPE_PONG = 129;
+export const ICMP_TYPE_ROUTER_SOLICITATION = 133;
+export const ICMP_TYPE_ROUTER_ADVERTISEMENT = 134;
+export const ICMP_TYPE_NEIGHBOR_SOLICITATION = 135;
+export const ICMP_TYPE_NEIGHBOR_ADVERTISEMENT = 136;
+export const ICMP_TYPE_REDIRECT = 137;
 
 export interface ICMPMessage {
 	type : number
@@ -28,6 +34,25 @@ export function calculateIcmp6Checksum( sourceAddress:IP6Address, destAddress:IP
 	return checksumming.digestAsUint16();
 }
 
+export function calculateIcmp6PacketChecksum( sourceAddress:IP6Address, destAddress:IP6Address, icmpPacket:Uint8Array ):number {
+	initIp6PseudoHeaderChecksumming( sourceAddress, destAddress, icmpPacket.length, ICMP_PROTOCOL_NUMBER );
+	icmpPacket[2] = 0;
+	icmpPacket[3] = 0;
+	checksumming.update(icmpPacket);
+	return checksumming.digestAsUint16();
+}
+
+/** In-place checksum calculation. */
+function fixIcmp6PacketChecksum( icmpPacket:Uint8Array, sourceAddress:IP6Address, destAddress:IP6Address ):void {
+	const checksum = calculateIcmp6PacketChecksum( icmpPacket, sourceAddress, destAddress );
+	icmpPacket[2] = checksum >> 8;
+	icmpPacket[3] = checksum >> 0;
+}
+
+/**
+ * Probably less efficient than building the packet in-place
+ * and then calculating the checksum with fixIcmp6PacketChecksum.
+ */
 export function assembleIcmp6Packet( icmpMessage:ICMPMessage, sourceAddress:IP6Address, destAddress:IP6Address ):Uint8Array {
 	if( icmpMessage.checksum == null ) {
 		icmpMessage.checksum = calculateIcmp6Checksum( sourceAddress, destAddress, icmpMessage );
@@ -49,3 +74,159 @@ export function disassembleIcmp6Packet( packet:Uint8Array ):ICMPMessage {
 		payload : new Uint8Array(packet.buffer, packet.byteOffset+4, packet.byteLength-4),
 	}
 }
+
+//// Neighbor discovery messages
+
+export const ND_OPTION_SOURCE_LINK_LAYER_ADDRESS = 1;
+export const ND_OPTION_TARGET_LINK_LAYER_ADDRESS = 2;
+export const ND_OPTION_PREFIX_INFORMATION = 3;
+export const ND_OPTION_REDIRECTED_HEADER = 4;
+export const ND_OPTION_MTU = 5;
+
+export interface PrefixInformation {
+	prefixLength : number;
+	onLink : boolean;
+	/** Indicates that this link can be used for stateless address configuration (see RFC 4682) */
+	autonomousAddressConfiguration : boolean;
+	/** infinity encoded as 0xffffffff */
+	validLifetime : number;
+	preferredLifetime : number;
+	prefix : Uint8Array;
+}
+
+export interface RouterAdvertisement {
+	/** Hop limit to use when sending packets; undefined encoded as 0 */
+	hopLimit : number;
+	/** Addresses are available via DHCPv6.  Meaningless if hasOtherConfiguration. */
+	hasManagedAddressConfiguration : boolean;
+	/** Other configuration is available via DHCPv6, including addresses */
+	hasOtherConfiguration : boolean;
+	/** Lifetime in seconds of the 'default router'; limit = 9000; undefined encoded as 0 */
+	routerLifetime? : number;
+	/** Expiration time (in milliseconds) of reachability confirmations; undefined encoded as 0 */
+	reachableTime? : number;
+	/** Time (in milliseconds) between neighbor solicitation retransmits; undefined encoded as 0 */
+	retransTime? : number;
+	
+	sourceLinkLayerAddress? : Uint8Array;
+	mtu? : number;
+	prefixInformation? : PrefixInformation;
+}
+
+function optSize( len:number ) {
+	return 8 * Math.ceil(len/8);
+}
+
+function encodeOptSize( len:number ) {
+	const v = Math.ceil(len/8);
+	if( v > 255 ) throw new Error("Can't encode option size; it is too big! "+len);
+	return v;
+}
+
+function infToInt32( a:number ):number {
+	return a == Infinity ? 0xFFFFFFFF : a;
+}
+
+function undefToZero( a:number|undefined ):number {
+	return a == undefined ? 0 : a;
+}
+
+function encodeLinkLayerAddressOption( type:number, address:Uint8Array, buffer:Uint8Array, dv:DataView, offset:number ):number {
+	buffer[offset+0] = type;
+	const wordCount = buffer[offset+1] = encodeOptSize(address.length+2);
+	buffer.set(address, offset+2);
+	return offset+wordCount*8;
+}
+function encodeMtuOption( mtu:number, buffer:Uint8Array, dv:DataView, offset:number ):number {
+	buffer[offset] = ND_OPTION_MTU;
+	buffer[offset+1] = 1;
+	dv.setUint32(offset+4, mtu);
+	return offset+8;
+}
+function encodePrefixInformationOption(pi:PrefixInformation, buffer:Uint8Array, dv:DataView, offset:number):number {
+	buffer[offset] = ND_OPTION_PREFIX_INFORMATION;
+	buffer[offset+1] = 4;
+	buffer[offset+2] = pi.prefixLength;
+	buffer[offset+3] = (pi.onLink ? 0x80 : 0) | (pi.autonomousAddressConfiguration ? 0x40 : 0);
+	dv.setUint32( offset+4, infToInt32(pi.validLifetime) );
+	dv.setUint32( offset+8, infToInt32(pi.preferredLifetime) );
+	buffer.set(pi.prefix, 16);
+	return offset+32;
+}
+
+export function assembleRouterAdvertisementPacket( ra:RouterAdvertisement, sourceAddress:Uint8Array, destAddress:Uint8Array ):Uint8Array {
+	let totalLength = 16; // including ICMP header
+	if( ra.sourceLinkLayerAddress ) totalLength += optSize(2+ra.sourceLinkLayerAddress.length);
+	if( ra.mtu != null ) totalLength += 8;
+	if( ra.prefixInformation ) totalLength += 32;
+	
+	const packet = new Uint8Array(totalLength);
+	const v = new DataView(packet.buffer, packet.byteOffset, packet.byteLength);
+	packet[0] = ICMP_TYPE_ROUTER_ADVERTISEMENT;
+	packet[1] = 0;
+	// checksum will go here
+	packet[4] = ra.hopLimit;
+	packet[5] = (ra.hasManagedAddressConfiguration ? 0x80 : 0) | (ra.hasOtherConfiguration ? 0x40 : 0);
+	v.setUint16( 6, undefToZero(ra.routerLifetime));
+	v.setUint32( 8, undefToZero(ra.reachableTime));
+	v.setUint32(12, undefToZero(ra.retransTime));
+	
+	let offset = 16;
+	if( ra.sourceLinkLayerAddress ) offset = encodeLinkLayerAddressOption(
+		ND_OPTION_SOURCE_LINK_LAYER_ADDRESS, ra.sourceLinkLayerAddress, packet, v, offset);
+	if( ra.mtu ) offset = encodeMtuOption(ra.mtu, packet, v, offset);
+	if( ra.prefixInformation ) offset = encodePrefixInformationOption(ra.prefixInformation, packet, v, offset);
+	
+	if( offset != totalLength ) throw new Error(
+		"Somehow offset didn't come out the same as precalculated total length "+
+		"(of router advertisement ICMP6 packet): "+offset+" != "+totalLength);
+	
+	fixIcmp6PacketChecksum( packet, sourceAddress, destAddress );
+	return packet;
+}
+
+/*
+Neighbor discovery options are encoded in such a way
+that functions to extract their 'payload' don't make much sense.
+
+export interface NeighborDiscoveryOption {
+	type : number;
+	packet? : Uint8Array;
+	payload? : Uint8Array;
+}
+
+export function encodeNeighborDiscoveryOptions( options:NeighborDiscoveryOption[], dest?:Uint8Array, offset:number=0 ):number {
+	for( let i in options ) {
+		const opt = options[i];
+		const pl = opt.payload;
+		const lengthInOctets = 2 + pl.length;
+		const lengthInWords = Math.ceil(length / 8);
+		if( lengthInWords > 255 ) throw new Error("Can't encode neighbor discovery option because payload is too long!");
+		if( opt.type < 0 || opt.type > 255 ) throw new Error("Invalid neighbor discovery option type: "+opt.type);
+		const endOffset = offset + lengthInWords;
+		if( dest ) {
+			dest[offset++] = opt.type;
+			dest[offset++] = lengthInWords;
+			for( let i = 0; i < pl.length; ++i ) {
+				dest[offset++] = pl[i];
+			}
+			while( offset < endOffset ) dest[offset++] = 0; // Padding
+		} else {
+			offset = endOffset;
+		}
+	}
+	return totalLength;
+}
+
+export function decodeNeighborDiscoveryOptions( encoded:Uint8Array ):NeighborDiscoveryOption[] {
+	if( encoded.length % 8 != 0 ) throw new PacketDecodeError("neighbor discovery options length not multiple of 8: "+encoded.length);
+	const opts : NeighborDiscoveryOption[] = [];
+	let offset = 0;
+	while( offset < encoded.length ) {
+		const type = encoded[offset];
+		const lengthInWords = encoded[offset+1];
+		const lengthInOctets = lengthInWords*8;
+		opts.push({ type: type, payload: new Uint8Array(encoded.buffer, encoded.offsetBytes+offset+2, lengthInOctets-2)});
+	}
+}
+*/
---
+++ src/main/ts/togos-game21/inet/icmp6test.ts
@@ -0,0 +1,28 @@
+import { hexEncode } from '../../tshash/utils';
+import { assertEqualsPromise, registerTestResult } from '../testing';
+import { parseIp6Address } from './IP6Address'
+import {
+	assembleRouterAdvertisementPacket,
+	PrefixInformation,
+	RouterAdvertisement
+} from './icmp6';
+
+// Uhm, let's try making a router advertisement packet.
+registerTestResult("icmp6test: simple router advertisement", new Promise( (resolve,reject) => {
+	const ra = {
+		hopLimit: 24,
+		hasManagedAddressConfiguration: false,
+		hasOtherConfiguration: false,
+	}
+	
+	const routerAddress = parseIp6Address('fe80:0002::1');
+	const hostAddress   = parseIp6Address('fe80:0002::2');
+	
+	const raIcmpPacket = assembleRouterAdvertisementPacket(ra, routerAddress, hostAddress);
+	if( raIcmpPacket.length == 0 ) {
+		resolve({errors:[new Error("Assembled packet has zero length!")]});
+		return;
+	}
+	// TODO: Parse packet, make sure everything's the same.
+	resolve({});
+}));
