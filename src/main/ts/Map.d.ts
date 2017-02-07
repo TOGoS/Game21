adad7eb10b083ebbb96b8ec7399109fb28bd3ca6
TOGoS <togos00@gmail.com>
2017-02-07 16:45:41 -0600

    Better Map definition.

--- src/main/ts/Map.d.ts
+++ src/main/ts/Map.d.ts
@@ -1,7 +1,11 @@
 declare class Map<K,V> {
-	get(k:K):V|undefined;
-	has(k:K):boolean;
-	set(k:K, v:V):Map<K,V>;
+	clear(): void;
+	delete(key: K): boolean;
+	forEach(callbackfn: (value: V, key: K, map: Map<K, V>) => void, thisArg?: any): void;
+	get(key: K): V | undefined;
+	has(key: K): boolean;
+	set(key: K, value?: V): this;
+	readonly size: number;
 }
 
 declare class WeakMap<K,V> {
