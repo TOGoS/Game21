81ccdebf7774ac69cd674514b93b20a019c94ff6
TOGoS <togos00@gmail.com>
2017-05-20 21:03:19 -0500

    Fix drawing, add wireless packet that flies around

--- src/main/ts/togos-game21/demo/SGWS.ts
+++ src/main/ts/togos-game21/demo/SGWS.ts
@@ -18,6 +18,12 @@
  */
 
 import EthernetSwitch, {EthernetSwitchSimulator} from '../netdev/EthernetSwitch';
+import { newType4Uuid } from 'tshash/uuids';
+import { hexEncode } from 'tshash/utils';
+
+function newUuidRef() {
+	return "urn:uuid:"+hexEncode(newType4Uuid());
+}
 
 enum Direction {
 	EAST = 0,
@@ -30,20 +36,25 @@ enum Direction {
 	NORTHEAST = 7
 }
 
+interface Vector2D { x:number, y:number }
+
+function directionToVector(dir:Direction):Vector2D {
+	switch( dir ) {
+	case Direction.EAST     : return {x:+1, y: 0};
+	case Direction.SOUTHEAST: return {x:+1, y:+1};
+	case Direction.SOUTH    : return {x:+0, y:+1};
+	case Direction.SOUTHWEST: return {x:-1, y:+1};
+	case Direction.WEST     : return {x:-1, y: 0};
+	case Direction.NORTHWEST: return {x:-1, y:-1};
+	case Direction.NORTH    : return {x: 0, y:-1};
+	case Direction.NORTHEAST: return {x:+1, y:-1};
+	}
+}
+
 function inverseDirection(dir:Direction):Direction {
 	return (dir + 4)&0x7;
 }
 
-interface Entity {
-	classRef: string;
-	draw(c2d:CanvasRenderingContext2D, x:number, y:number, scale:number):void;
-}
-
-interface TreeEntity {
-	classRef: "http://ns.nuke24.net/Game21/SGWS/Tree",
-	// TODO: different species, ages, sizes
-	// trees should grow larger with age and suck nutrients from the ground
-}
 
 interface ItemClass {
 	volume : number;
@@ -119,13 +130,58 @@ interface EntitySimulator<Entity> {
 	packetReceived( entity:Entity, originDirection:Direction, packet:Packet, ws:SGWorldSimulator ):void;
 }
 
-const entitySimulators:{[entityClassRef:string]: EntitySimulator<any>} = {
-	//"http://ns.nuke24.net/Game21/SGWS/NetworkDevice": new NetworkDeviceEntity,
+interface Thing {
+	draw(c2d:CanvasRenderingContext2D, x:number, y:number, scale:number):void;
+	tick(sim:SGWorldSimulator):void;
 }
 
-const EMPTY_STACK:Entity[] = [];
+class Entity implements Thing {
+	// These properties managed by the simulator; don't change unless you are the simulator!
+	// Use methods on simulator instead.
+	id : string;
+	x : number;
+	y : number;
+	active : boolean;
+	
+	constructor(props:{id?:string}={}) {
+		this.id = props.id || newUuidRef();
+	}
+	
+	draw(c2d:CanvasRenderingContext2D, x:number, y:number, scale:number):void { }
+	tick(sim:SGWorldSimulator):void { }
+}
 
-class Block implements Entity {
+class WirelessPacket extends Entity {
+	velocity : Vector2D;
+	data : Packet;
+	
+	constructor(props:{id?:string, velocity:Vector2D, data?:Packet}) {
+		super(props);
+		this.active = true;
+		this.velocity = props.velocity;
+		this.data = props.data || new Uint8Array(0);
+	}
+	
+	draw(c2d:CanvasRenderingContext2D, x:number, y:number, scale:number):void {
+		c2d.fillStyle = 'rgba(255,255,192,0.75)';
+		c2d.fillRect(x-scale/4, y-scale/4, scale/2, scale/2);
+	}
+	tick(sim:SGWorldSimulator):void {
+		sim.moveEntity(this, this.x+this.velocity.x, this.y+this.velocity.y);
+		// TODO: Check if absorbed by something or something
+		sim.markEntityActive(this);
+	}
+}
+
+interface TreeEntity {
+	classRef: "http://ns.nuke24.net/Game21/SGWS/Tree",
+	// TODO: different species, ages, sizes
+	// trees should grow larger with age and suck nutrients from the ground
+}
+
+const EMPTY_STACK:Thing[] = [];
+
+class Block implements Thing {
 	classRef = "http://ns.nuke24.net/Game21/SGWS/Block";
 	public draw(c2d:CanvasRenderingContext2D, x:number, y:number, scale:number):void {
 		c2d.fillStyle = 'rgb(0,0,0)';
@@ -133,6 +189,7 @@ class Block implements Entity {
 		c2d.fillStyle = 'rgb(255,255,255)';
 		c2d.fillRect(x-scale/2+1, y-scale/2+1, scale-2, scale-2);
 	}
+	tick(sim:SGWorldSimulator):void { }
 }
 
 class SGWorldSimulator {
@@ -140,7 +197,8 @@ class SGWorldSimulator {
 	protected height:number;
 	protected xMask:number;
 	protected yMask:number;
-	protected entityStacks:Entity[][];
+	protected thingStacks:Thing[][];
+	protected activeEntities:{[k:string]: Entity} = {};
 	
 	constructor( public widthBits:number, public heightBits:number ) {
 		this.width = 1 << widthBits;
@@ -148,22 +206,69 @@ class SGWorldSimulator {
 		this.height = 1 << heightBits;
 		this.yMask = this.height-1;
 		
-		this.entityStacks = new Array(this.width*this.height);
-		for( let i=this.width*this.height-1; i>=0; --i ) this.entityStacks[i] = EMPTY_STACK;
+		this.thingStacks = new Array(this.width*this.height);
+		for( let i=this.width*this.height-1; i>=0; --i ) this.thingStacks[i] = EMPTY_STACK;
 	}
 	
-	public addEntity( x:number, y:number, entity:Entity ) {
-		let index = x+this.width*y;
-		if( this.entityStacks[index] === EMPTY_STACK ) {
-			this.entityStacks[index] = [entity];
+	public addThing( thing:Thing, x:number, y:number ):void {
+		let index = (x&this.xMask)+this.width*(y&this.yMask);
+		if( this.thingStacks[index] === EMPTY_STACK ) {
+			this.thingStacks[index] = [thing];
 		} else {
-			this.entityStacks[index].push(entity);
+			this.thingStacks[index].push(thing);
+		}
+		if( thing instanceof Entity ) {
+			thing.x = x;
+			thing.y = y;
+			if( thing.active ) this.markEntityActive(thing);
+		}
+	}
+	
+	public removeThing( entity:Thing, x:number, y:number ):void {
+		let index = (x&this.xMask)+this.width*(y&this.yMask);
+		let stack = this.thingStacks[index];
+		for( let i=0; i<stack.length; ++i ) {
+			if( stack[i] === entity ) {
+				if( stack.length == 1 ) {
+					this.thingStacks[index] = EMPTY_STACK;
+				} else {
+					stack.splice(i,1);
+					return;
+				}
+			}
 		}
 	}
 	
 	public canvas : HTMLCanvasElement;
 	public tick():void {
 		this.drawScene();
+		let activeEntities = this.activeEntities;
+		this.activeEntities = {};
+		for( let i in activeEntities ) {
+			activeEntities[i].active = false;
+			activeEntities[i].tick(this);
+		}
+	}
+	
+	public markEntityActive(entity:Entity) {
+		entity.active = true;
+		this.activeEntities[entity.id] = entity;
+	}
+	
+	public moveEntity(entity:Entity, x:number, y:number) {
+		const oldX = Math.floor(entity.x), oldY = Math.floor(entity.y);
+		const newX = x&this.xMask, newY = y&this.yMask;
+		if( oldX == newX && oldY == newY ) {
+			// TODO: Mod between 0..width/height
+			entity.x = x;
+			entity.y = y;
+			return;
+		}
+		
+		this.removeThing(entity, oldX, oldY);
+		this.addThing(entity, newX, newY);
+		entity.x = x;
+		entity.y = y;
 	}
 	
 	public drawScene():void {
@@ -173,7 +278,8 @@ class SGWorldSimulator {
 		let canvWidth = this.canvas.width;
 		let canvHeight = this.canvas.height;
 		
-		c2d.clearRect(0,0,canvWidth,canvHeight);
+		c2d.fillStyle = 'rgb(0,0,0)';
+		c2d.fillRect(0,0,canvWidth,canvHeight);
 		
 		let drawScale = 16;
 		let drawCenterX = 8;
@@ -188,12 +294,12 @@ class SGWorldSimulator {
 		
 		for( let y=y0; y<y1; ++y ) {
 			for( let x=x0; x<x1; ++x ) {
-				let idx = (x&xMask)+(y&yMask)*this.width;
-				let stack = this.entityStacks[idx];
+				let index = (x&xMask)+(y&yMask)*this.width;
+				let stack = this.thingStacks[index];
 				for( let i=0; i<stack.length; ++i ) {
 					stack[i].draw(c2d,
-						canvWidth/2 + drawScale * (x-drawCenterX),
-						canvHeight/2 + drawScale * (x-drawCenterY),
+						canvWidth /2 + drawScale * (x-drawCenterX),
+						canvHeight/2 + drawScale * (y-drawCenterY),
 						drawScale
 					);
 				}
@@ -214,6 +320,10 @@ class SGWSDemo {
 export function createDemo(canvas:HTMLCanvasElement) {
 	const demo:SGWSDemo = new SGWSDemo();
 	demo.canvas = canvas;
-	demo.sim.addEntity(1, 2, new Block());
+	const block = new Block();
+	demo.sim.addThing(block, 0, 2);
+	demo.sim.addThing(block, 1, 2);
+	demo.sim.addThing(block, 1, 3);
+	demo.sim.addThing(new WirelessPacket({velocity:{x:0,y:1}}), 2, 2);
 	return demo;
 }
